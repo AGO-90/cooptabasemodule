e817c8c63be87b0e5e047f6ffce545c86018066f
AGO-90 <andres.ghione@gmail.com>
2023-02-18 16:11:18 -0300

    delete person from driver

--- components/driver.go
+++ components/driver.go
@@ -3,7 +3,6 @@ package components
 type Driver struct {
 	ID                    int64  `gorm:"primaryKey" json:"id"`
 	CooperativeIdentifier string `json:"cooperative_identifier"`
-	Person                Person `gorm:"foreignKey:DocumentNumber" json:"person"`
 	PersonDocumentNumber  string `json:"person_document_number"`
 }
 
