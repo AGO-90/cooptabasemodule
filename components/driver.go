3bc230947800b7a5cf927b199cca8af703556cb2
AGO-90 <andres.ghione@gmail.com>
2023-02-18 06:14:53 -0300

    foreign key

--- components/driver.go
+++ components/driver.go
@@ -6,7 +6,7 @@ type Driver struct {
 	gorm.Model
 	ID                    int64  `gorm:"primaryKey" json:"id"`
 	CooperativeIdentifier string `json:"cooperative_identifier"`
-	Person                Person `gorm:"foreignKey:PeopleDocumentNumber" json:"person"`
+	Person                Person `gorm:"foreignKey:people_document_number" json:"person"`
 }
 
 func (Driver) TableName() string {
