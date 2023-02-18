f945f61b9331af39d85d997045f928897de35a00
AGO-90 <andres.ghione@gmail.com>
2023-02-18 06:21:40 -0300

    add person document number to driver

--- components/driver.go
+++ components/driver.go
@@ -6,7 +6,8 @@ type Driver struct {
 	gorm.Model
 	ID                    int64  `gorm:"primaryKey" json:"id"`
 	CooperativeIdentifier string `json:"cooperative_identifier"`
-	Person                Person `gorm:"foreignKey:people_document_number" json:"person"`
+	Person                Person `gorm:"foreignKey:DocumentNumber" json:"person"`
+	PersonDocumentNumber  string `json:"person_document_number"`
 }
 
 func (Driver) TableName() string {
